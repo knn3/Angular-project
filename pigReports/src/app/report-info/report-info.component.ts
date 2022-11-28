71f30e873ed1b71a78f57b40a8bfd84555ec0254
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-11-28 00:02:13 -0800

    Updated report more info

--- pigReports/src/app/report-info/report-info.component.ts
+++ pigReports/src/app/report-info/report-info.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
+import { HttpClient } from '@angular/common/http';
+
+import { Report } from '../report/report.service';
 
 
 @Component({
@@ -9,10 +12,17 @@ import { ActivatedRoute } from '@angular/router'
 })
 export class ReportInfoComponent implements OnInit {
   id: any;
-  constructor(private ActivatedRoute: ActivatedRoute) { }
+  report: Report;
+  constructor(private ActivatedRoute: ActivatedRoute, private http: HttpClient) { 
+    this.report = new Report()
+  }
 
   ngOnInit(): void {
     this.id = this.ActivatedRoute.snapshot.paramMap.get('id')
+    this.http.get(`https://272.selfip.net/apps/DHt1XR53QT/collections/reports/documents/${this.id}/`)
+      .subscribe((data: any) => {
+        this.report = data.data
+    })
   }
 
 }
--- pigReports/src/app/report/report.service.ts
+++ pigReports/src/app/report/report.service.ts
@@ -5,40 +5,26 @@ import { Inject, inject, Injectable } from '@angular/core';
 })
   
 export class Report {
-  // name: string;
-  // phoneNum: number;
-  // breed: string;
-  // Pid: number;
-  // location: string;
-  // longitude: number;
-  // latitude: number;
-  // extraNotes: string;
-  // date: number;
-  // time: number;
-  // status: boolean;
+  name: string = "";
+  phoneNum: number = 0;
+  breed: string = "";
+  Pid: number = 0;
+  location: string = "";
+  longitude: number= 0;
+  latitude: number= 0;
+  extraNotes: string ="";
+  date: number= 0;
+  time: number= 0;
+  status: boolean = false;
 
-  // constructor(@Inject('n') n: string, @Inject('pN') pN: number, b:string, id: number, l: string, long: number, la: number, extra: string, s:boolean) {
-  //   this.name = n;
-  //   this.phoneNum = pN;
+  constructor() {
     
-  //   this.breed = b;
-  //   this.Pid = id;
-
-  //   this.location = l;
-  //   this.longitude = long;
-  //   this.latitude = la;
-  //   this.extraNotes = extra;
-
-  //   this.date = new Date().getTime();
-  //   this.time = new Date().getTime();
-
-  //   this.status = s
-  //  }
+   }
 
   // reports = [];
 
   // addNewReport(name: string, phoneNum: number, breed: string, Pid: number, location: string, longitude: number, latitude: number, extraNotes: string, date: number, time: number, status: boolean) {
     
-  
+
 
 }
